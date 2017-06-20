000cbedbfb338b8605e2a84dc645f5a11c2f7dbe
celerZhao <zhaowenzhen1986@126.com>
2017-06-20 19:26:14 +0800

    bwo

--- js/bOilPlugIn/bOilPlugIn.js
+++ js/bOilPlugIn/bOilPlugIn.js
@@ -717,6 +717,7 @@ bOilPlugIn.prototype = {
                 if (strl > 4) {
                     thisVal = thisVal.replace(/(\d{4})(?=\d)/g, '$1 ');
                     $(this).val(thisVal);
+					alert(111)
 					_thisEl.get(0).setSelectionRange(thisVal.length-3,thisVal.length);
 					//_thisEl.focus();
 					//_thisEl.focus();
